8e663991e7ad40d8465cbb12ae9e8a034554f5df
Ema <emanuele.piantoni@studiofarma.it>
2013-08-06 11:22:05 +0200

    castle windsor

---
+++ NancyApplicationExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NancyApplicationExample.WebApiApplication" Language="C#" %>
---
+++ NancyApplicationExample/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using NancyApplicationExample.App;
+
+namespace NancyApplicationExample
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            IWindsorContainer container = new WindsorContainer();
+            container.Install(FromAssembly.This());
+            GlobalConfiguration.Configuration.DependencyResolver = new WindsorDependencyResolver(container);
+        }
+
+        protected void Application_Error(Object sender, EventArgs e)
+        {
+
+        }
+    }
+}
